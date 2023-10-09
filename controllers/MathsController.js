dff101cd544e031a102e5773a35841530d3d0c59
Florence1234567 <119359132+Florence1234567@users.noreply.github.com>
2023-10-09 15:31:50 -0400

    .

---
+++ controllers/MathsController.js
@@ -0,0 +1,22 @@
+import Controller from './Controller.js';
+
+export default class MathsController extends Controller {
+    write() {
+        let url = new URL(window.location.toLocaleString());
+        let urlParams = queryStringParser.parse(url);
+        let urlParts = path.split("/");
+
+    }
+    addition() {
+
+    }
+    substraction() {
+
+    }
+    multiplication() {
+
+    }
+    division() {
+
+    }
+}
